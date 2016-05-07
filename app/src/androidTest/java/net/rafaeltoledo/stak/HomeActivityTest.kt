194a03e6a9a6ded16f55ec5ed82568baaca40672
Rafael Toledo <rafaeldtoledo@gmail.com>
2016-05-07 08:10:53 -0300

    Downgraded RequestMatcher, as its last version doesn't work well with Kotlin

--- app/src/androidTest/java/net/rafaeltoledo/stak/HomeActivityTest.kt
+++ app/src/androidTest/java/net/rafaeltoledo/stak/HomeActivityTest.kt
@@ -1,7 +1,6 @@
 package net.rafaeltoledo.stak
 
 import android.content.Intent
-import android.os.SystemClock
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
@@ -9,7 +8,8 @@ import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtP
 import android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import android.support.test.espresso.intent.Intents
 import android.support.test.espresso.intent.Intents.intended
-import android.support.test.espresso.intent.matcher.IntentMatchers.*
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasAction
+import android.support.test.espresso.intent.matcher.IntentMatchers.hasData
 import android.support.test.espresso.intent.matcher.UriMatchers
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
@@ -18,7 +18,8 @@ import br.com.concretesolutions.requestmatcher.InstrumentedTestRequestMatcherRul
 import net.rafaeltoledo.stak.data.api.ApiCaller
 import net.rafaeltoledo.stak.ui.UserViewHolder
 import net.rafaeltoledo.stak.ui.activity.HomeActivity
-import org.hamcrest.CoreMatchers.*
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -47,23 +48,18 @@ class HomeActivityTest {
                 .assertHasQuery("sort", "reputation")
                 .assertHasQuery("site", "stackoverflow")
 
-        val expectedIntent = allOf(
-                hasAction(Intent.ACTION_VIEW),
-                hasCategories(hasItem(equalTo(Intent.CATEGORY_BROWSABLE))),
-                hasData(UriMatchers.hasHost(equalTo("http://stackoverflow.com/users/157882/balusc"))))
-
         Intents.init();
         activityRule.launchActivity(Intent(Intent.ACTION_MAIN))
 
         onView(withId(R.id.recyclerView))
                 .perform(scrollToPosition<UserViewHolder>(3))
 
-        SystemClock.sleep(10000)
-
         onView(withId(R.id.recyclerView))
                 .perform(actionOnItemAtPosition<UserViewHolder>(2, click()));
 
-        intended(expectedIntent);
+        intended(allOf(hasAction(Intent.ACTION_VIEW),
+                hasData(UriMatchers.hasHost(equalTo("stackoverflow.com")))
+        ));
         Intents.release();
     }
 }
